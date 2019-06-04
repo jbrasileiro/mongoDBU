6f1ffd583031da01300ae9ad4a385e82c56f6ef5
João Carlos Brasileiro <jcbrasileiro@hotmail.com>
2019-06-03 23:04:02 -0300

    Chapter 3: Admin Backend - Ticket: User Report

--- M220J/mflix/src/main/java/mflix/api/daos/CommentDao.java
+++ M220J/mflix/src/main/java/mflix/api/daos/CommentDao.java
@@ -4,6 +4,7 @@ import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -20,9 +21,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.mongodb.MongoClientSettings;
+import com.mongodb.ReadConcern;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
 import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
@@ -155,12 +159,15 @@ public class CommentDao extends AbstractMFlixDao {
    */
   public List<Critic> mostActiveCommenters() {
     List<Critic> mostActive = new ArrayList<>();
-    // // TODO> Ticket: User Report - execute a command that returns the
-    // // list of 20 users, group by number of comments. Don't forget,
-    // // this report is expected to be produced with an high durability
-    // // guarantee for the returned documents. Once a commenter is in the
-    // // top 20 of users, they become a Critic, so mostActive is composed of
-    // // Critic objects.
+		Bson count = Aggregates.sortByCount("$email");
+		Bson limit = Aggregates.limit(20);
+		Bson sort = Aggregates.sort(Sorts.descending("count"));
+		List<Bson> pipeline = Arrays.asList(count, limit, sort);
+		commentCollection
+			.withReadConcern(ReadConcern.MAJORITY)
+			.aggregate(pipeline, Critic.class)
+			.iterator()
+			.forEachRemaining(mostActive::add);
     return mostActive;
   }
 }
